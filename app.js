bca5bef5431c7e103038343b52b9ea53ba50a98d
Sayeed Tahseen <sayeed.tahseen1@t-mobile.com>
2021-10-04 12:36:51 -0400

    Added books controller, added routes for GET, POST and DELETE endpoints and logic

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const app = express();
+const books = require('./books/booksController');
 
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/api/books', books);
 
 app.get("/health", (req, res) => {
   res.status(200).send("Don't panic.");
 });
-
-
 
 module.exports = app;
---
+++ books/booksController.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const books = express();
+const preExistingBooks = require('./preExistingBooks.json');
+
+booksStored = preExistingBooks;
+
+books.get('/', (req, res) => {
+    booksStored.sort( (a,b) => {
+        if ( a.title < b.title )
+            return -1
+        else if ( a.title >= b.title)
+            return 1
+    })
+
+    res.status(201).json(booksStored)
+})
+
+books.post('/', (req, res) => {
+    let bookToStore = req.body;
+    bookToStore.id = booksStored.length + 1;
+    booksStored.push(bookToStore);
+
+    res.status(200).json(bookToStore)
+})
+
+books.delete('/', (req, res) => {
+    booksStored = [];
+    res.sendStatus(204);
+})
+
+module.exports = books;
