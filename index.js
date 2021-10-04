920ed85b391a45c9bdaedd1ef59a48641dbf18ac
Sayeed Tahseen <sayeed.tahseen1@t-mobile.com>
2021-10-04 14:04:50 -0400

    Updated port number do dynamically be allocated

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const port = process.env.port || 5000;
+const port = process.env.port;
 const app = require('./app');
 
-app.listen(port, '0.0.0.0', () => {
+app.listen(port, () => {
   console.log(`Server is running, listening on port ${port}`);
 });
