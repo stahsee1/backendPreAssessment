7f3eb478b8c6c94a37e5440c646caab3c2207bb1
Sayeed Tahseen <sayeed.tahseen1@t-mobile.com>
2021-10-04 13:27:47 -0400

    Changed port number to use env variable on heroku

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const port = 5000;
 const app = require('./app');
 
-app.listen(port, () => {
+app.listen(process.env.port || port, () => {
   console.log(`Server is running, listening on port ${port}`);
 });
